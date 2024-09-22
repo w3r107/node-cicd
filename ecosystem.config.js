8366e5f93104b2e3cf9c7b86951b8bd38c0d4d7b
tushar02 <tushark@krishworks.com>
2024-09-22 11:56:21 +0530

    asd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     production: {
       user: "w3r107",
       host: "172.25.68.239",
-      path: "/home/w3r107/webdev/node-cidcd",
+      path: "/home/w3r107/webdev/node-cidcd1",
       repo: "git@github.com:w3r107/react-cicd.git",
       ref: "origin/main",
       key: "/home/w3r107/.ssh/id_ed25519",
